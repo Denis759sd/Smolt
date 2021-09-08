960b4e39134699d58c31337c5c8cc39135cb7cef
oll_Ti_Mist <kotickpro@gmail.com>
2021-09-08 22:39:48 +0300

    add Yandex Metrica

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -13,6 +13,21 @@
           rel="stylesheet">
     <!-- Template CSS -->
 
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(85037503, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/85037503" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
     @yield('custom_js')
 </head>
 
